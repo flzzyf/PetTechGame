2dd44c00ee1d8a31435a4f335c98589faa9acb5f
flzzyf <2981019122@qq.com>
2018-08-14 12:48:57 +0800

    加入Vuforia操控代码，还有狗狗

---
+++ PetTechGame/Assets/Scripts/VuforiaManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using Vuforia;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class HitTestResultEvent : UnityEvent<HitTestResult> { }
+public class VuforiaManager : MonoBehaviour
+{
+    public HitTestResultEvent clickEvent;
+
+    public Text text;
+
+    bool created = false;
+
+    public void OnInteractiveHitTest(HitTestResult result)
+    {
+        if (!created)
+            clickEvent.Invoke(result);
+    }
+
+    bool hit = false;
+    public void Hit()
+    {
+        hit = true;
+    }
+
+    private void LateUpdate()
+    {
+        text.text = "侦测到平面：" + hit.ToString();
+
+        hit = false;
+    }
+}
