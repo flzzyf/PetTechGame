0e06bafd55da776be4ae05e138fde9c32f32a28c
flzzyf <2981019122@qq.com>
2018-08-15 21:43:19 +0800

    导入鸡腿，嗯

--- PetTechGame/Assets/Scripts/GameManager.cs
+++ PetTechGame/Assets/Scripts/GameManager.cs
@@ -11,6 +11,8 @@ public class GameManager : Singleton<GameManager>
 
     public Transform cam;
 
+    public Transform groundPanel;
+
     void Start()
     {
 
@@ -19,6 +21,7 @@ public class GameManager : Singleton<GameManager>
     void Update()
     {
         SetText("镜头坐标", cam.position + "");
+        SetText("平面坐标", groundPanel.position + "");
     }
 
     public void SetText(string _key, string _text)
