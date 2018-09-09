3d3906b259c9ddc9425f37e2a5c163e8cec59922
flzzyf <2981019122@qq.com>
2018-09-09 14:41:40 +0800

    改成黑帅

--- PetTechGame/Assets/Scripts/Dog.cs
+++ PetTechGame/Assets/Scripts/Dog.cs
@@ -12,6 +12,15 @@ public class Dog : MonoBehaviour
     public float stat_hungry = 5;
     public float speed = 3;
 
+    public Animator animator;
+
+    public Transform gfx;
+
+    void Start()
+    {
+        StartCoroutine(BarkSometimes());
+    }
+
     void Update()
     {
         if (GameManager.instance.interactableobjects.Count == 0)
@@ -22,15 +31,30 @@ public class Dog : MonoBehaviour
             SearchTarget();
         else
         {
-            //有目标，超过交互范围，靠近
-            if (HorizontalDistance(transform.position, target.position) > range_interact)
+            //有目标
+            Vector3 dir = target.position - transform.position;
+
+            //设置朝向
+            if (Vector3.Dot(transform.right, dir.normalized) > 0)
             {
-                Vector3 dir = target.position - transform.position;
-                dir.y = 0;
-                transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+                GetComponentInChildren<SpriteRenderer>().flipX = true;
             }
             else
             {
+                GetComponentInChildren<SpriteRenderer>().flipX = false;
+            }
+            //超过交互范围，靠近
+            if (HorizontalDistance(transform.position, target.position) > range_interact)
+            {
+                dir.y = 0;
+                transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+                animator.SetBool("walking", true);
+            }
+            else
+            {
+                animator.SetBool("walking", false);
+
                 if (Vector3.Distance(transform.position, target.position) < range_interact)
                 {
                     //目标在交互范围内
@@ -64,9 +88,20 @@ public class Dog : MonoBehaviour
 
     void Eat(GameObject _target)
     {
+        SoundManager.instance.Play("Eat");
+
         target = null;
         GameManager.instance.interactableobjects.Remove(_target.transform);
         Destroy(_target);
+    }
+
+    IEnumerator BarkSometimes()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(3, 5));
+            SoundManager.instance.Play("Bark");
+        }
     }
 
 }
---
+++ PetTechGame/Assets/Scripts/Util/SoundManager.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class SoundManager : Singleton<SoundManager>
+{
+    public Sound[] sounds = new Sound[1];
+
+    private void Awake()
+    {
+        Init();
+    }
+
+    public void Init() 
+	{
+        foreach (Sound s in sounds)
+        {
+            if (s.multipleSound)
+                continue;
+
+            CreateSoundComponent(s);
+        }
+    }
+
+    AudioSource CreateSoundComponent(Sound s)
+    {
+        s.source = gameObject.AddComponent<AudioSource>();
+        if (s.audioMixerGroup != null)
+            s.source.outputAudioMixerGroup = s.audioMixerGroup;
+        s.source.clip = s.clip;
+        s.source.volume = s.volume;
+        s.source.pitch = s.pitch;
+        s.source.loop = s.loop;
+
+        return s.source;
+    }
+
+    public AudioSource Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return null;
+        }
+
+        if(!s.multipleSound)
+            s.source.Play();
+        else
+        {
+            //同时可能有多个的声音，而且会单独关闭
+            AudioSource source = CreateSoundComponent(s);
+            source.Play();
+            return source;
+        }
+
+        return null;
+    }
+
+    public void StopPlay(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+
+        s.source.Stop();
+    }
+}
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume = 1;
+    [Range(.1f, 3f)]
+    public float pitch = 1;
+
+    public bool loop;
+
+    [HideInInspector]
+    public AudioSource source;
+
+    public AudioMixerGroup audioMixerGroup;
+
+    public bool multipleSound;
+}
+
